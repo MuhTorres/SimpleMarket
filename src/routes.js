7c716a90727c4c3830d79f03eedf0b995d2f9984
muliro <murilo@xnetweb.com.br>
2019-07-27 00:44:07 -0300

    Tratamento de exceções

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const handle = require('express-async-handler');
 const UserController = require('./app/controllers/UserController');
 const SalesOrderController = require('./app/controllers/SalesOrderController');
 const ItemController = require('./app/controllers/ItemController');
@@ -6,26 +7,26 @@ const Middleware = require('./app/middlewares/Auth');
 
 const routes = express.Router();
 
-routes.post('/users', UserController.create);
-routes.post('/login', UserController.authorize);
+routes.post('/users', handle(UserController.create));
+routes.post('/login', handle(UserController.authorize));
 
 routes.use(Middleware);
 
-routes.get('/users', UserController.list);
-routes.get('/users/:id', UserController.get);
-routes.put('/users/:id', UserController.update);
-routes.delete('/users/:id', UserController.destroy);
+routes.get('/users', handle(UserController.list));
+routes.get('/users/:id', handle(UserController.get));
+routes.put('/users/:id', handle(UserController.update));
+routes.delete('/users/:id', handle(UserController.destroy));
 
-routes.get('/sales', SalesOrderController.list);
-routes.get('/sales/:id', SalesOrderController.get);
-routes.post('/sales', SalesOrderController.create);
-routes.put('/sales/:id', SalesOrderController.update);
-routes.delete('/sales/:id', SalesOrderController.destroy);
+routes.get('/sales', handle(SalesOrderController.list));
+routes.get('/sales/:id', handle(SalesOrderController.get));
+routes.post('/sales', handle(SalesOrderController.create));
+routes.put('/sales/:id', handle(SalesOrderController.update));
+routes.delete('/sales/:id', handle(SalesOrderController.destroy));
 
-routes.get('/items', ItemController.list);
-routes.get('/items/:id', ItemController.get);
-routes.post('/items', ItemController.create);
-routes.put('/items/:id', ItemController.update);
-routes.delete('/items/:id', ItemController.destroy);
+routes.get('/items', handle(ItemController.list));
+routes.get('/items/:id', handle(ItemController.get));
+routes.post('/items', handle(ItemController.create));
+routes.put('/items/:id', handle(ItemController.update));
+routes.delete('/items/:id', handle(ItemController.destroy));
 
 module.exports = routes;
